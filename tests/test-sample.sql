01ee0171fad5fea1de7a560dadb980f2b5d78bcb
rch-liu <rachel98rl@hotmail.com>
2020-05-22 23:48:49 -0400

    Updated tests

--- tests/test-sample.sql
+++ tests/test-sample.sql
@@ -6,3 +6,12 @@ WHERE films.start_year > 1894 AND
       ratings.num_votes >= 10
 ORDER BY ratings.average_ratings DESC
 LIMIT 10
+
+SELECT films.original_title, films.is_adult, ratings.num_votes
+FROM ratings, films
+WHERE films.runtime_minutes > 1 AND
+      films.is_adult = FALSE AND
+      ratings.tconst = films.tconst AND
+      ratings.num_votes >= 30
+ORDER BY ratings.average_ratings ASC
+LIMIT 10
